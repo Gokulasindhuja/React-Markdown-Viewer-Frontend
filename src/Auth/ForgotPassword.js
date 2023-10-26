f859bdae314943378d38180f9364d33306232ffc
Gokulasindhuja <136049113+Gokulasindhuja@users.noreply.github.com>
2023-10-26 20:32:35 +0530

    Update ForgotPassword.js

--- src/Auth/ForgotPassword.js
+++ src/Auth/ForgotPassword.js
@@ -7,7 +7,6 @@ import { Link, useNavigate } from "react-router-dom";
 import { useFormik } from "formik";
 import * as yup from "yup";
 import {API} from "../globle"  
-import load from "../assets/loading4.svg";
 
 
 const formValidationSchema = yup.object({
