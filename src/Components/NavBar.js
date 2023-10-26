f29a72105e9365f1539684ce6a119ae63ee30359
Gokulasindhuja <136049113+Gokulasindhuja@users.noreply.github.com>
2023-10-26 20:45:57 +0530

    Update NavBar.js

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import ReactRouterBootstrap, { LinkContainer } from 'react-router-bootstrap';
 import Button from 'react-bootstrap/Button';
 import {LogOut} from "../Auth/LogOut";
 
